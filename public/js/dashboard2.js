c9426d819312be791fd97358af70673c3206676c
Akshat Shukla <shuklaakshatascom@gmail.com>
2025-01-24 22:18:49 +0530

    Fixed favicons, ejs discord link, theme retention

--- public/js/dashboard2.js
+++ public/js/dashboard2.js
@@ -1,17 +1,25 @@
 const dark = document.querySelector(".dark");
 const light = document.querySelector(".light");
-const logo = document.getElementById("mainLogo");
 
 dark.addEventListener("click", function () {
   document.querySelector("body").classList.add("darkMode");
   light.classList.remove("active");
   dark.classList.add("active");
-  logo.setAttribute('src','../public/img/iwoc_logo_light.png');
+  localStorage.setItem("theme", "dark");
 });
 
 light.addEventListener("click", function () {
   document.querySelector("body").classList.remove("darkMode");
   dark.classList.remove("active");
   light.classList.add("active");
-  logo.setAttribute('src','../public/img/iwoc_logo_dark.png');
+  localStorage.setItem("theme", "light");
 });
+
+window.onload = function () {
+  const theme = localStorage.getItem("theme");
+  if (theme === "dark") {
+    document.querySelector("body").classList.add("darkMode");
+    light.classList.remove("active");
+    dark.classList.add("active");
+  }
+}
--- routes/index.js
+++ routes/index.js
@@ -42,91 +42,91 @@ const Exists = async (id) => {
   return false;
 }
 
-// router.get("/register",
-//   async (req, res, next) => {
-//     if (req.session.passport && (await Exists(req.session.passport.user))) next();
-//     else res.sendFile(path.join(__dirname, "../pages/form.html"));
-//   },
-//   async (req, res, next) => {
-//     if (await Registered(req.session.passport.user)) res.redirect("/dashboard");
-//     else next();
-//   },
-//   (req, res, next) => {
-//     res.sendFile(path.join(__dirname, "../pages/form.html"));
-//   }
-// );
+router.get("/register",
+  async (req, res, next) => {
+    if (req.session.passport && (await Exists(req.session.passport.user))) next();
+    else res.sendFile(path.join(__dirname, "../pages/form.html"));
+  },
+  async (req, res, next) => {
+    if (await Registered(req.session.passport.user)) res.redirect("/dashboard");
+    else next();
+  },
+  (req, res, next) => {
+    res.sendFile(path.join(__dirname, "../pages/form.html"));
+  }
+);
 
-// router.post("/register",
-//   async (req, res) => {
-//     if (validateData(req.body)) {
-//       console.log(req.body);
+router.post("/register",
+  async (req, res) => {
+    if (validateData(req.body)) {
+      console.log(req.body);
       
-//       const resp = await UserHandler.addUser(req.body);
-//       res.send(JSON.stringify(resp));
-//     }
-//     else{
-//       const resp = {message: "Invalid Data"};
-//       res.send(JSON.stringify(resp));
-//     } 
-//   }
-// );
+      const resp = await UserHandler.addUser(req.body);
+      res.send(JSON.stringify(resp));
+    }
+    else{
+      const resp = {message: "Invalid Data"};
+      res.send(JSON.stringify(resp));
+    } 
+  }
+);
 
-// router.get("/login",
-//   async (req, res, next) => {
-//     if (req.session.passport && (await Exists(req.session.passport.user))) next();
-//     else res.sendFile(path.join(__dirname, "../pages/login.html"));
-//   },
-//   async (req, res, next) => {
-//     if (await Registered(req.session.passport.user)) res.redirect("/dashboard");
-//     else next();
-//   },
-//   (req, res, next) => {
-//     res.sendFile(path.join(__dirname, "../pages/login.html"));
-//   }
-// );
+router.get("/login",
+  async (req, res, next) => {
+    if (req.session.passport && (await Exists(req.session.passport.user))) next();
+    else res.sendFile(path.join(__dirname, "../pages/login.html"));
+  },
+  async (req, res, next) => {
+    if (await Registered(req.session.passport.user)) res.redirect("/dashboard");
+    else next();
+  },
+  (req, res, next) => {
+    res.sendFile(path.join(__dirname, "../pages/login.html"));
+  }
+);
 
-// router.get("/dashboard",
-//   async (req, res, next) => {
-//     if (req.session.passport && (await Exists(req.session.passport.user))) next();
-//     else res.redirect("/login");
-//   }, 
-//   async (req, res, next) => {
-//     if (await Registered(req.session.passport.user)) next();
-//     else{
-//       await User.deleteOne({_id:req.session.passport.user});
-//       res.redirect("/unauthenticated"); 
-//     } 
-//   },
-//   async (req, res, next) => {
-//     const user = await User.findById(req.session.passport.user);
-//     res.render("dashboard", { user: user });
-//   }
-// );
+router.get("/dashboard",
+  async (req, res, next) => {
+    if (req.session.passport && (await Exists(req.session.passport.user))) next();
+    else res.redirect("/login");
+  }, 
+  async (req, res, next) => {
+    if (await Registered(req.session.passport.user)) next();
+    else{
+      await User.deleteOne({_id:req.session.passport.user});
+      res.redirect("/unauthenticated"); 
+    } 
+  },
+  async (req, res, next) => {
+    const user = await User.findById(req.session.passport.user);
+    res.render("dashboard", { user: user });
+  }
+);
 
-// router.get("/dashboard/leaderboard",
-//   async (req, res, next) => {
-//     if (req.session.passport && (await Exists(req.session.passport.user))) next();
-//     else res.redirect("/login");
-//   },
-//   async (req, res, next) => {
-//     if (await Registered(req.session.passport.user)) next();
-//     else{
-//       await User.deleteOne({_id:req.session.passport.user});
-//       res.redirect("/unauthenticated");
-//     } 
-//   },
-//   async (req, res, next) => {
-//     const user_t = await User.findById(req.session.passport.user);
-//     const users = await User.find();
-//     await users.sort(function(a, b){return b.score - a.score});
-//     const rank = users.map(e => e.username).indexOf(user_t.username) + 1;
-//     if(req.query.user){
-//       const user = await User.findOne({username:req.query.user});
-//       res.render("history", { user_t:user_t,user: user,rank:rank });
-//     }
-//     else res.render("leaderboard", { user: user_t,users: users, rank:rank});
-//   }
-// );
+router.get("/dashboard/leaderboard",
+  async (req, res, next) => {
+    if (req.session.passport && (await Exists(req.session.passport.user))) next();
+    else res.redirect("/login");
+  },
+  async (req, res, next) => {
+    if (await Registered(req.session.passport.user)) next();
+    else{
+      await User.deleteOne({_id:req.session.passport.user});
+      res.redirect("/unauthenticated");
+    } 
+  },
+  async (req, res, next) => {
+    const user_t = await User.findById(req.session.passport.user);
+    const users = await User.find();
+    await users.sort(function(a, b){return b.score - a.score});
+    const rank = users.map(e => e.username).indexOf(user_t.username) + 1;
+    if(req.query.user){
+      const user = await User.findOne({username:req.query.user});
+      res.render("history", { user_t:user_t,user: user,rank:rank });
+    }
+    else res.render("leaderboard", { user: user_t,users: users, rank:rank});
+  }
+);
 
 // router.get("/logout", (req, res, next) => {
 //   req.logout((err) => {
@@ -135,9 +135,9 @@ const Exists = async (id) => {
 //   });
 // });
 
-// router.get("/unauthenticated", async (req, res, next) => {
-//   res.sendFile(path.join(__dirname, "../pages/unauthenticated.html"));
-// });
+router.get("/unauthenticated", async (req, res, next) => {
+  res.sendFile(path.join(__dirname, "../pages/unauthenticated.html"));
+});
 
 // // ************************ --------------------- *********************************
 
